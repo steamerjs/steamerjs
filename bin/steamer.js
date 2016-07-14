18e02b75b0c678d604824309966a90980ea12be1
lcxfs1991@gmail.com <lcxfs1991@gmail.com>
2016-07-14 12:44:18 +0800

    first commit

---
+++ bin/steamer.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+"use strict";
+
+const fs = require('fs'),
+	  path = require('path'),
+	  argv = require('yargs').argv,
+	  _ = require('lodash'),
+	  Err = require('steamer-core').Err,
+	  Conf = require('steamer-core').Conf;
+
+
+const pkgPrefix = 'steamer-plugin-';
+
+function Commander() {
+
+};
+
+Commander.prototype.initPlugin = function() {
+	// command example: steamer init
+	let mainCommands = argv._,
+		pkg = null,
+		plugin = null;
+	
+	if (mainCommands.length) {
+		// use the 1st value
+		pkg = pkgPrefix + mainCommands[0];
+
+		try {
+			plugin = require(pkg);
+
+			if (_.has(plugin, 'init') && _.isFunction(plugin.init)) {
+				plugin.init();
+			}
+		}
+		catch(e) {
+			if (e.code === 'MODULE_NOT_FOUND') {
+				Err.PluginNotFond(pkg);
+			}
+		}
+	} 
+};
+
+Commander.prototype.init = function() {
+	this.initPlugin();
+};
+
+var commander = new Commander();
+commander.init();
+
+
+module.exports = Commander;
+
+
+
